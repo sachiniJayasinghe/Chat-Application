ac29e7a7d32ea7b5bd7ff4b4b8f9847da083a108
“sachiniJayasinghe” <gunasekarasachini2003@gmail.com>
2024-01-31 19:46:55 +0530

    updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     //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
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("hi");
     }
 }
