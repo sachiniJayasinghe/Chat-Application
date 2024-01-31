4b5dbce35c43bf7f4d1a9551eb23d02abdb1afc5
“sachiniJayasinghe” <gunasekarasachini2003@gmail.com>
2024-01-31 21:08:41 +0530

    Created the login form

--- src/main/java/Ijse/lk/controller/LoginController.java
+++ src/main/java/Ijse/lk/controller/LoginController.java
@@ -1,4 +1,31 @@
 package Ijse.lk.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LoginController {
+
+    @FXML
+    private TextField txtname;
+    static String username;
+
+    @FXML
+    void NameOnAction(ActionEvent event) throws IOException {
+        username=txtname.getText();
+        txtname.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LoginController.class.getResource("/view/ClientForm.fxml"))));
+        stage.setTitle("TalkLia");
+        stage.getIcons().add(new Image("/img/preview 2.png"));
+        stage.centerOnScreen();
+        stage.show();
+    }
+
 }
--- src/main/java/Ijse/lk/server/Server.java
+++ src/main/java/Ijse/lk/server/Server.java
@@ -1,4 +1,5 @@
 package Ijse.lk.server;
 
 public class Server {
+
 }
